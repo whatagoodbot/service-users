d48c0bc5e56787172f5f874eeb224d8a9ceb2a09
notABluesSinger <rob@whatagoodbot.com>
2022-11-04 10:27:10 +0000

    Moar Robust Defense

--- src/controllers/getUsers.js
+++ src/controllers/getUsers.js
@@ -1,8 +1,9 @@
 import { usersDb } from '../models/index.js'
 
 export const getUser = async (call, callback) => {
-  if (call?.request && call?.request?.id) {
+  if (call && call.request && call.request.id) {
     const user = await usersDb.get(call?.request?.id)
+    if (!user) callback(null, null)
     callback(null, user)
   } else {
     callback(null, null)
